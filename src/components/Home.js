0aae8dd208837ac10e3a8c04329569ed0e81732c
Nathaniel Ring <nathanielring@gmail.com>
2017-01-19 14:02:55 -0500

    Fixing time

--- src/components/Home.js
+++ src/components/Home.js
@@ -40,7 +40,7 @@ class Home extends Component {
 
         <EventBlock
           title="Ceremony and Reception"
-          date="Saturday, August 26, 2017"
+          date="Saturday, August 26, 2017 - 5:00PM"
           image="http://www.mcginleykalsow.com/projects/images/commanding.jpg">
           <p>440 Talcott Ave<br />Watertown, MA 02472</p>
           <p>Join us for a night of delicious food and dancing at the Commander's Mansion in Watertown.</p>
@@ -48,7 +48,7 @@ class Home extends Component {
 
         <EventBlock
           title="Shuttle from Hotel to Venue"
-          date="Saturday, August 26, 2017 - 4:00PM"
+          date="Saturday, August 26, 2017 - 4:15PM"
           image="http://www.rustywalrus.com/images/lg/8q.jpg">
             <p>Boston Marriott Cambridge<br />50 Broadway, Cambridge, MA 02142</p>
             <p>The shuttle will be leaving at 4:15pm sharp and will return to Kendall Square after the wedding. Please RSVP to let us know whether you're interested in getting a ride to/from Kendall Square. We'll follow up separately once we're closer to the date.</p>
