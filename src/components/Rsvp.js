a2072b3690c2bde2a9b528daf709e37f94a76c86
Nathaniel Ring <nathanielring@gmail.com>
2017-01-19 14:03:01 -0500

    Hiding RSVP form

--- src/components/Rsvp.js
+++ src/components/Rsvp.js
@@ -11,7 +11,8 @@ class Rsvp extends Component {
   render() {
     return (
       <div>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScW03qUAzq-g_uDJOT2gxQ8oqOuUBlS9OFd3nlyHzB5Bf-IJQ/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+        <h1 style={{ "textAlign":"center", "padding": "5rem"}}>RSVP Form will be right back!</h1>
+        {/*<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScW03qUAzq-g_uDJOT2gxQ8oqOuUBlS9OFd3nlyHzB5Bf-IJQ/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>*/}
       </div>
     )
   }
