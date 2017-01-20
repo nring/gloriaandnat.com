37fe2c17b25dc7d301b7bb9e6a259fc838c41d3f
Nathaniel Ring <nathanielring@gmail.com>
2017-01-20 17:46:14 -0500

    Re-adding RSVP

--- src/components/Rsvp.js
+++ src/components/Rsvp.js
@@ -11,8 +11,7 @@ class Rsvp extends Component {
   render() {
     return (
       <div>
-        <h1 style={{ "textAlign":"center", "padding": "5rem"}}>RSVP Form will be right back!</h1>
-        {/*<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScW03qUAzq-g_uDJOT2gxQ8oqOuUBlS9OFd3nlyHzB5Bf-IJQ/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>*/}
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScW03qUAzq-g_uDJOT2gxQ8oqOuUBlS9OFd3nlyHzB5Bf-IJQ/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
       </div>
     )
   }
